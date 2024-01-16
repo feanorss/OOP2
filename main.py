bb3a41614bab4c90e2e48258502e426f8bfe0c30
feanorss <madro.milos@gmail.com>
2024-01-16 18:47:38 +0100

    Polymorfia

--- main.py
+++ main.py
@@ -8,3 +8,39 @@ class MojaKalkulacka:
 
 print(MojaKalkulacka.sucet(2,2))
 print(MojaKalkulacka.sucin(2,3))
+
+class Zviera:
+    def hlas(self):
+        raise NotImplementedError("Podtrida musi implementovat tuto metodu")
+
+class Pes(Zviera):
+    def hlas(self):
+        return "haf"
+    def koncatiny(self):
+        return "4"
+
+class Kohut(Zviera):
+    def hlas(self):
+        return "Kotkodak"
+    def koncatiny(self):
+        return "2"
+
+class Macka(Zviera):
+    def hlas(self):
+        return "Mnau"
+    def koncatiny(self):
+        return "5"
+
+def vydaj_zvuk(Zviera):
+    return zviera.hlas()
+
+def napis_koncatiny(Zviera):
+    return zviera.koncatiny()
+
+pes = Pes()
+macka = Macka()
+kohut = Kohut()
+
+for zviera in [pes, macka, kohut]:
+    print(vydaj_zvuk(zviera))
+    print(napis_koncatiny(zviera))

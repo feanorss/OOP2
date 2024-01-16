c134cf1d8cf7cebd8b6b52541a56fa74a40aca4f
feanorss <madro.milos@gmail.com>
2024-01-16 20:59:07 +0100

    posledna zmena

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ stadion3=Stadium("Lombardi",2000,"IT","Bologna",5000)
 stadions=[stadion1,stadion2,stadion3]
 
 zoradenie=sorted(stadions, key=lambda x: x.capacity)
-print(zoradenie)
+
 
 for stadium in zoradenie:
   print(f"{stadium.name} - Capacity: {stadium.capacity}")
@@ -78,3 +78,26 @@ for stadium in zoradenie:
 #         najvacsi_stadion = i
 #
 # print(najvacsi_stadion.name)
+
+class Book:
+    def __init__(self, title, pages, price):
+        self.title = title
+        self.pages = pages
+        self.__price = price
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self,value):
+        if value >= 0:
+            self.__price = value
+        else:
+            raise ValueError("Price is negative")
+
+kniha = Book("Harry Potter", 400, 10)
+print(kniha.price)
+kniha.price = 20
+print(kniha.price)
+

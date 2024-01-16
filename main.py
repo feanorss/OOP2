57101e56972851b58404f07e769eaba3361dbee0
feanorss <madro.milos@gmail.com>
2024-01-16 20:36:25 +0100

    zoradenie

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ class Macka(Zviera):
     def koncatiny(self):
         return "5"
 
+
+
 def vydaj_zvuk(Zviera):
     return zviera.hlas()
 
@@ -44,3 +46,35 @@ kohut = Kohut()
 for zviera in [pes, macka, kohut]:
     print(vydaj_zvuk(zviera))
     print(napis_koncatiny(zviera))
+
+class Stadium:
+  def __init__(self, name, opening, country, city, capacity):
+      self.name=name
+      self.opening=opening
+      self.country=country
+      self.city=city
+      self.capacity=capacity
+
+
+
+
+stadion1=Stadium("Tipos",2015,"SR","Bratislava",15000)
+stadion2=Stadium("Steal arena", 2010,"SR","Kosice",10000)
+stadion3=Stadium("Lombardi",2000,"IT","Bologna",5000)
+
+stadions=[stadion1,stadion2,stadion3]
+
+zoradenie=sorted(stadions, key=lambda x: x.capacity)
+print(zoradenie)
+
+for stadium in zoradenie:
+  print(f"{stadium.name} - Capacity: {stadium.capacity}")
+
+# najvacsia_kapacita=0
+# najvacsi_stadion=None
+# for i in stadions:
+#     if i.capacity>najvacsia_kapacita:
+#         najvacsia_kapacita = i.capacity
+#         najvacsi_stadion = i
+#
+# print(najvacsi_stadion.name)
